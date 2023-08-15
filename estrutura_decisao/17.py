f584f3601ddfadd0226003b721846abceb89985b
AndreGorny <andre.gorny@gmail.com>
2023-08-15 16:43:03 -0300

    2023-08-15

--- estrutura_decisao/17.py
+++ estrutura_decisao/17.py
@@ -1,6 +1,31 @@
 # Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a
 # mesma é uma data válida.
 
-data = input("Insira uma data (dd/mm/aaa): ")
 
-tem que terminar
+def ano_bissexto(ano):
+    if ano % 4 != 1:
+        return True
+    else:
+        return False
+
+
+mes_31 = [1, 3, 5, 7, 8, 10, 12]
+
+while 1:
+    data = input("Insira uma data (dd/mm/aaa): ")
+    dia, mes, ano = data.split("/")
+
+    if ano_bissexto(ano) is True:
+        if mes == 2 and dia > 29:
+            print("Data inválida. Repita")
+
+"""    if mes in mes_31 and dia > 30:
+        print("Data inválida")
+    elif mes == 2 and dia > 29:
+        pass
+
+    dia, mes, ano = data.split("/")
+
+print(dia, mes, ano)"""
+
+#corrije saporra
